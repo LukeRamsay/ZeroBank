4d5f585ff8bb5f7eb6472f9bbc2969d1136ecf35
LukeRamsay <180075@virtualwindow.co.za>
2020-08-28 14:01:10 +0200

    Added search functionality
    Not yet working but have commented out the code

--- src/main/kotlin/com/example/demo/view/crud/SearchFragment.kt
+++ src/main/kotlin/com/example/demo/view/crud/SearchFragment.kt
@@ -1,4 +1,26 @@
 package com.example.demo.view.crud
 
+import com.example.demo.model.Account
+
 class SearchFragment {
+
+    /*private var baseList: List<Account> = listOf()
+    private var filteredList: MutableList<Account> = mutableListOf()
+
+    fun searchList() {
+        filteredList.clear()
+        baseList.forEach {
+            if (it.contains(search.text.toString())) {
+                filteredList.add(it)
+            }
+        }
+        adapter.updateList(filteredList)
+    }
+    fun updateList(filteredList: List<Account>) {
+        list.clear()
+        list.addAll(filteredList)
+        notifyDataSetChanged()
+    }*/
+
+
 }
